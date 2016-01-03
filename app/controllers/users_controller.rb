72dadff216648b34e9a91e145bd4df9fcc6a570a
aspsa <123aspsa123@gmail.com>
2016-01-03 00:25:14 +0000

    Change private wikis to public wikis on Blocipedia account downgrade.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,12 @@ class UsersController < ApplicationController
     current_user.update(role: 'standard') unless current_user.role == 'standard'
     
     if current_user.update(role: 'standard')
-      flash[:notice] = "You have elected to downgrade your account to the standard level. Feel free to upgrade your account to a premium level at any time in order to enjoy the full Blocipedia benefits!"
+      flash[:notice] = "You have elected to downgrade your account to the standard level. Please note that your private wiki entries will be converted into public wiki entries. Feel free to upgrade your account to a premium level at any time in order to enjoy the full Blocipedia benefits!"
+      
+      current_user.wikis.each do |wiki|
+        wiki.update(private: false)
+      end
+      
       redirect_to user_path(current_user)
     else
       flash[:error] = "Your account failed to downgrade."
