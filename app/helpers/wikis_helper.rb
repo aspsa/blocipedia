82350577801fbe326e88c5e9969346c3ca81176b
aspsa <123aspsa123@gmail.com>
2016-01-17 21:02:31 +0000

    Fix broken new wiki creation capability.

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,11 +1,15 @@
 module WikisHelper
   def wiki_collaboration_link(wiki, user)
+    return unless wiki.id
+    
     # If this wiki entry does not have a user id in the collaborators table, then this user is a potential collaborator.
     unless wiki.collaborator?(user)
-      link_to "Collaborate", add_collaborator_wiki_path(wiki: wiki, user: user.id)
+      #link_to "Collaborate", add_collaborator_wiki_path(wiki: wiki, user: user.id)
+      link_to "Collaborate", add_collaborator_wiki_path(wiki: wiki, user: user)
     # If this wiki entry has a user id in the collaborators table, then this user is a candidate for removal as a collaborator.
     else
-      link_to "Remove Collaboration", delete_collaborator_wiki_path(wiki: wiki, user: user.id)
+      #link_to "Remove Collaboration", delete_collaborator_wiki_path(wiki: wiki, user: user.id)
+      link_to "Remove Collaboration", delete_collaborator_wiki_path(wiki: wiki, user: user)
     end
   end
   
